37f6d3b85a8ec1b5d69f9246d451f7150593b390
Cindy Bohorquez <cinaboho@espol.edu.ec>
2021-08-21 11:48:08 -0500

    Comentario
    Solo prueba

--- Codigo Proyecto.R
+++ Codigo Proyecto.R
@@ -1,10 +1,10 @@
-title: 'Proyecto de Estadística: Análisis de Pruebas de Hepatitis tipo C'
+title: 'Proyecto de Estad?stica: An?lisis de Pruebas de Hepatitis tipo C'
 author: "CRISTIAN ARREAGA , DIEGO BERREZUETA , CINDY BOHORQUEZ , JEIMY QUIMI  "
 date: "11/21/2020"
 
 ---
   
-  #Proyecto de Estadística: Análisis de Pruebas de Hepatitis tipo C
+  #Proyecto de Estad?stica: An?lisis de Pruebas de Hepatitis tipo C
   
   # llamamos al archivo
 
@@ -14,13 +14,14 @@ data = read.csv(url("https://archive.ics.uci.edu/ml/machine-learning-databases/0
 data
 
 # Variables 
+#cambio
 
 categoria= data$Category  # CAtegoria de pacientes segun Enfermedad
 sexo= data$Sex     # Sexo del Paciente
 edad= data$Age    # Edad del paciente
 
 # Primer Objetivo :
-#Verificar si existe asociación estadística entre el tipo de la enfermedad y el sexo de la persona.
+#Verificar si existe asociaci?n estad?stica entre el tipo de la enfermedad y el sexo de la persona.
 
 #Siendo:
 # H0: El tipo de enfermedad es independiente del sexo
@@ -31,7 +32,7 @@ edad= data$Age    # Edad del paciente
 
 
 
-#2. Verificar si existe asociación estadística entre el tipo de la enfermedad y la edad de la persona.
+#2. Verificar si existe asociaci?n estad?stica entre el tipo de la enfermedad y la edad de la persona.
 
 #Siendo:
 #H0: El tipo de enfermedad es independiente de la edad.
