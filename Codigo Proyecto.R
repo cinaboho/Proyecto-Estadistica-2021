b1717ad7ccbd025511a350ea6f86a743e6347298
Cindy Bohorquez <cinaboho@espol.edu.ec>
2021-08-22 00:15:16 -0500

    Creacion de 4 Tablas Enfermedad/Edades
    .

--- Codigo Proyecto.R
+++ Codigo Proyecto.R
@@ -87,8 +87,103 @@ cant3<-data.frame(cant_porEdad_vejez)
 colnames(cant3)<-c("edad_Vejez","No Personas")
 cant3
 
-
-
-
-
-
+#------------Segundo Paso----------#
+#Separo la columna Categorias de la base de
+#datos a un datafram de una 1 sola columna
+categorias<-bd$Category
+categoria=data.frame(categorias)
+#Hago un bucle for para crear una tabla con
+#cada categoria de enfermedad y las respectivas
+#edades de los infectados
+X<-categoria[1, ]
+Y<-edad[1,]
+for(i in 1:dim(categoria)[1]){
+    #c<-categoria[i, ]
+ 
+  if(categoria[i,]=="0=Blood Donor"){
+    X<- cbind(X,categoria[i,])
+    Y<- cbind(Y,edad[i,])
+    
+  }
+}
+#Z es el data frame de enfermedades
+#K es el data framde de edades
+#Utilizo merge para unir la tabla Z y K
+Z=data.frame(X)
+K=data.frame(Y)
+J=merge(x = Z, y = K,all = TRUE)
+#Para una mejor visualizacion,
+#lo muestro de forma vertical y finalmente
+#imprimo la tabla completa.
+tabla_bloodDonor<-t(J)
+tabla_bloodDonor
+#Ahora haremos lo mismo para las demas 
+#categorias de enfermedades
+#------------0s=suspect Blood Donor-------------#
+M<-categoria[1, ]
+N<-edad[1,]
+for(i in 1:dim(categoria)[1]){
+  #c<-categoria[i, ]
+  
+  if(categoria[i,]=="0s=suspect Blood Donor"){
+    M<- cbind(M,categoria[i,])
+    N<- cbind(N,edad[i,])
+    
+  }
+}
+O=data.frame(M)
+P=data.frame(N)
+Q=merge(x = O, y = P,all = TRUE)
+tabla_SuspectbloodDonor<-t(Q)
+tabla_SuspectbloodDonor
+#------------1=Hepatitis-------------#
+A<-categoria[1, ]
+B<-edad[1,]
+for(i in 1:dim(categoria)[1]){
+  #c<-categoria[i, ]
+  
+  if(categoria[i,]=="1=Hepatitis"){
+    A<- cbind(A,categoria[i,])
+    B<- cbind(B,edad[i,])
+    
+  }
+}
+C=data.frame(A)
+D=data.frame(B)
+W=merge(x = C, y = D,all = TRUE)
+tabla_Hepatitis<-t(W)
+tabla_Hepatitis
+#------------2=Fibrosis-------------#
+S<-categoria[1, ]
+U<-edad[1,]
+for(i in 1:dim(categoria)[1]){
+  #c<-categoria[i, ]
+  
+  if(categoria[i,]=="2=Fibrosis"){
+    S<- cbind(S,categoria[i,])
+    U<- cbind(U,edad[i,])
+    
+  }
+}
+I=data.frame(S)
+G=data.frame(U)
+LL=merge(x = I, y = G,all = TRUE)
+tabla_Fibrosis<-t(LL)
+tabla_Fibrosis
+#-----------3=Cirrhosis-------------#
+S1<-categoria[1, ]
+U1<-edad[1,]
+for(i in 1:dim(categoria)[1]){
+  #c<-categoria[i, ]
+  
+  if(categoria[i,]=="3=Cirrhosis"){
+    S1<- cbind(S1,categoria[i,])
+    U1<- cbind(U1,edad[i,])
+    
+  }
+}
+I1=data.frame(S1)
+G1=data.frame(U1)
+LL1=merge(x = I1, y = G1,all = TRUE)
+tabla_Cirrhosis<-t(LL1)
+tabla_Cirrhosis
